04eb068e3e48d08a34a5035da1f878e403262f3d
Gonron <lundsgaard1997@hotmail.dk>
2020-02-06 12:08:08 +0100

    init assignment 01-UnionFind

---
+++ Assignments/01-UnionFind/src/QuickUnion.java
@@ -0,0 +1,24 @@
+public class QuickUnion implements UnionFind{
+    public QuickUnion() {
+    }
+
+    @Override
+    public void union(int p, int q) {
+
+    }
+
+    @Override
+    public int find(int p) {
+        return 0;
+    }
+
+    @Override
+    public boolean connected(int p, int q) {
+        return false;
+    }
+
+    @Override
+    public int count() {
+        return 0;
+    }
+}
