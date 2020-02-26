a1556a177d3822e14c912b4d9a02c5344c8fb443
Gonron <lundsgaard1997@hotmail.dk>
2020-02-26 10:52:51 +0100

    -

---
+++ Exercises/4.01-PriorityQueue/src/util/NodeIterator.java
@@ -0,0 +1,24 @@
+package util;
+
+import java.util.Iterator;
+
+public class NodeIterator<T> implements Iterator<T> {
+    private Node<T> step ;
+
+    public NodeIterator (Node<T> first ) {
+        this . step = first ;
+    }
+
+    @Override
+    public boolean hasNext () {
+        return step != null;
+    }
+
+    @Override
+    public T next () {
+        T value = step.getValue();
+        step = step.getNext();
+        return value ;
+    }
+}
+
