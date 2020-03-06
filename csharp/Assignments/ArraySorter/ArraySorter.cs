966c78d563a32e1b3bc5196c8dbc77e8c915a2c8
Gonron <lundsgaard1997@hotmail.dk>
2020-03-06 14:04:26 +0100

    Finished ArraySorter

--- csharp/Assignments/ArraySorter/ArraySorter.cs
+++ csharp/Assignments/ArraySorter/ArraySorter.cs
@@ -1,33 +1,110 @@
 ﻿using System;
+using System.Collections;
 
 namespace ArraySorter {
-    public class ArraySorter<T> {
-        private T[] items;
-        private int size;
+    public class ArraySorter<T> where T : IComparable<T> {
+        public T[] Queue { get; }
+        public int HeapSize { get; private set; }
+        private bool _isSortedAscending;
+        private int MaxHeapSize { get; }
 
-        public ArraySorter(T[] items, int size) {
-            this.items = items;
-            this.size = size;
+        public ArraySorter(T[] items, int heapSize) {
+            Queue = new T[heapSize];
+            HeapSize = items.Length;
+            MaxHeapSize = heapSize;
+            // Copying the context of itemsArray to the Queue
+            for (var i = 0; i < items.Length; i++) {
+                Queue[i] = items[i];
+            }
         }
 
         public void Enqueue(T item) {
-            throw new NotImplementedException();
+            if (HeapSize == MaxHeapSize) {
+                throw new Exception("Queue is full");
+            }
+
+            Queue[HeapSize++] = item;
+
+            if (_isSortedAscending) {
+                BuildHeap((x, y) => x.CompareTo(y) < 0);
+            }
+            else {
+                BuildHeap((x, y) => x.CompareTo(y) > 0);
+            }
         }
 
         public T Dequeue() {
-            throw new NotImplementedException();
+            if (HeapSize == 0) {
+                throw new Exception("Queue is empty, can't dequeue.");
+            }
+
+            var root = Queue[0];
+
+            // Move last element to the root
+            Queue[0] = Queue[--HeapSize];
+            Queue[HeapSize] = default;
+
+            if (_isSortedAscending) {
+                BuildHeap((x, y) => x.CompareTo(y) < 0);
+            }
+            else {
+                BuildHeap((x, y) => x.CompareTo(y) > 0);
+            }
+
+            return root;
         }
 
         public void SortAscending() {
-            throw new NotImplementedException();
+            Sort((x, y) => x.CompareTo(y) > 0);
         }
 
         public void SortDescending() {
-            throw new NotImplementedException();
+            Sort((x, y) => x.CompareTo(y) < 0);
         }
 
-        public void Sort(IComparable T) {
-            throw new NotImplementedException();
+        private void Sort(Func<T, T, bool> lambda) {
+            var length = HeapSize;
+            BuildHeap(lambda);
+            for (var i = length - 1; i >= 0; i--) {
+                // Move current root to end
+                Exch(Queue, 0, i);
+                // Heapify the reduced heap
+                Heapify(Queue, i, 0, lambda);
+            }
+
+            // Get the sorting order by comparing root to the last element
+            _isSortedAscending = Queue[0].CompareTo(Queue[HeapSize - 1]) < 0;
+        }
+
+        private void BuildHeap(Func<T, T, bool> lambda) {
+            for (var i = HeapSize / 2 - 1; i >= 0; i--) {
+                Heapify(Queue, HeapSize, i, lambda);
+            }
+        }
+
+        public void Heapify(T[] arr, int size, int i, Func<T, T, bool> lambda) {
+            var parent = i;
+            var leftChild = i * 2 + 1;
+            var rightChild = i * 2 + 2;
+
+            if (leftChild < size && lambda(arr[leftChild], arr[parent])) {
+                parent = leftChild;
+            }
+
+            if (rightChild < size && lambda(arr[rightChild], arr[parent])) {
+                parent = rightChild;
+            }
+
+            if (parent != i) {
+                Exch(arr, i, parent);
+                Heapify(arr, size, parent, lambda);
+            }
+        }
+
+        private void Exch(T[] arr, int a, int b) {
+            var temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
         }
     }
 }
--- csharp/Assignments/ArraySorter/Program.cs
+++ csharp/Assignments/ArraySorter/Program.cs
@@ -3,7 +3,35 @@
 namespace ArraySorter {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            var input = new[] {100, 30, 20, 90, 80, 70, 40, 60, 50, 10};
+            var arrSorter = new ArraySorter<int>(input, 20);
+            
+            Console.Write("Sorting descending: ");
+            arrSorter.SortDescending();
+            foreach (var item in arrSorter.Queue) {
+                Console.Write($"{item.ToString()} ");
+            }
+            
+            Console.Write(("\n\nSorting ascending: "));
+            arrSorter.SortAscending();
+            foreach (var item in arrSorter.Queue) { 
+                Console.Write($"{item.ToString()} ");
+            }
+            
+            Console.Write("\n\nRemoving all elements: ");
+            while (arrSorter.HeapSize > 0) {
+                Console.Write($"\n Removing: {arrSorter.Dequeue().ToString()} \n New Array: ");
+                foreach (var item in arrSorter.Queue)
+                {
+                    Console.Write(item.ToString() + " ");
+                }
+            }
+            
+            arrSorter.Enqueue(120);
+            Console.WriteLine("\nInserting 120");
+            Console.WriteLine($"Dequeuing {arrSorter.Dequeue().ToString()}");
         }
+        
+        // AdamSai is my hero
     }
 }
